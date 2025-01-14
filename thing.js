d6be587920b16926484fd0fced557e24d36772dc
MaxM2559 <93101107+MaxM2559@users.noreply.github.com>
2025-01-13 21:50:34 -0500

    A few changes

---
+++ thing.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    const container = document.querySelector('.container');
+    container.classList.add('fade-in'); // Add class to trigger the animation
+  });
